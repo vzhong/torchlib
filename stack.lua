12a946b3b9e88cd4a302ce3e9789007e7bd3a81a
Victor Zhong <victor@victorzhong.com>
2015-08-05 11:22:23 -0700

    add stack

---
+++ stack.lua
@@ -0,0 +1,12 @@
+Stack = torch.class('Stack', 'LinkedList')
+
+function Stack:push(val)
+  self:add(val)
+end
+
+function Stack:pop()
+  assert(self:size() > 0, 'cannot dequeue from empty queue')
+  return self:remove(self:size())
+end
+
+torch.getmetatable('Stack').__tostring__ = Stack.toString
---
+++ test/test_stack.lua
@@ -0,0 +1,28 @@
+require 'torchlib'
+
+local TestStack = {}
+local tester
+
+function TestStack.testStack()
+  local s = Stack.new()
+  tester:asserteq(0, s:size())
+
+  s:push(10)
+  tester:asserteq(1, s:size())
+  s:push(20)
+  tester:asserteq(2, s:size())
+
+  tester:asserteq('Stack[10, 20]', tostring(s))
+
+  v = s:pop()
+  tester:asserteq(20, v)
+  tester:asserteq(1, s:size())
+  v = s:pop()
+  tester:asserteq(10, v)
+  tester:asserteq(0, s:size())
+end
+
+
+tester = torch.Tester()
+tester:add(TestStack)
+tester:run()
