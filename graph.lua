efed13e049d998221aa048ef7c0c1570e5cc3157
Victor Zhong <victor@victorzhong.com>
2015-08-07 14:07:40 -0700

    add BFS, shortest path

--- graph.lua
+++ graph.lua
@@ -1,6 +1,8 @@
 local Graph = torch.class('Graph')
 Graph.GraphNode = torch.class('GraphNode')
 
+Graph.state = {UNDISCOVERED = 1, VISITED = 2, FINISHED = 3}
+
 function Graph.GraphNode:__init(val)
   self.val = val
 end
@@ -30,9 +32,65 @@ function Graph:addNode(val)
 end
 
 function Graph:connectionsOf(node)
+  -- returns a table list of nodes that are children to node
   self:assertValidNode(node)
-  return self._nodeMap:get(node):asTable()
+  return self._nodeMap:get(node):toTable()
 end
+
+function Graph:nodeSet()
+  return self._nodeMap:keySet()
+end
+
+function Graph:breadthFirstSearch(source)
+  -- initialize all nodes to undiscovered, source to visited
+  local nodes = self:nodeSet():toTable()
+  for i = 1, #nodes do
+    local node = nodes[i]
+    node.state = Graph.state.UNDISCOVERED
+    node.timestamp = math.huge
+    node.parent = nil
+  end
+  source.state = Graph.state.VISITED
+  source.timestamp = 0
+  
+  local q = Queue.new()
+  q:enqueue(source)
+
+  while not q:isEmpty() do
+    local node = q:dequeue()
+    conns = self:connectionsOf(node)
+    for i = 1, #conns do
+      local conn = conns[i]
+      if conn.state == Graph.state.UNDISCOVERED then
+        conn.state = Graph.state.VISITED
+        conn.timestamp = node.timestamp + 1
+        conn.parent = node
+        q:enqueue(conn)
+      end
+    end
+    node.state = Graph.state.FINISHED
+  end
+end
+
+function Graph:shortestPath(source, destination, skipBFS)
+  if skipBFS ~= true then
+    self:breadthFirstSearch(source)
+  end
+  local function recur(graph, source, destination, path)
+    if source == destination then
+      table.insert(path, destination)
+    elseif destination.parent == nil then
+      error('Error: no path from ' .. tostring(source) .. ' to ' .. tostring(destination))
+    else
+      recur(graph, source, destination.parent, path)
+      table.insert(path, destination)
+    end
+  end
+  local path = {}
+  recur(self, source, destination, path)
+  return path
+end
+
 
 local DirectedGraph = torch.class('DirectedGraph', 'Graph')
 
--- test/test_graph.lua
+++ test/test_graph.lua
@@ -32,6 +32,82 @@ function TestUndirectedGraph.testAddNode()
   tester:assertTableEq({na}, g:connectionsOf(nc))
 end
 
+function getUndirectedGraph()
+  -- figure 22.3 from CLRS
+  local g = UndirectedGraph.new()
+  local r = g:addNode('r')
+  local s = g:addNode('s')
+  local t = g:addNode('t')
+  local u = g:addNode('u')
+  local v = g:addNode('v')
+  local w = g:addNode('w')
+  local x = g:addNode('x')
+  local y = g:addNode('y')
+
+  g:connect(v, r)
+  g:connect(r, s)
+  g:connect(s, w)
+  g:connect(w, t)
+  g:connect(w, x)
+  g:connect(t, x)
+  g:connect(t, u)
+  g:connect(x, u)
+  g:connect(x, y)
+  g:connect(y, u)
+
+  return g, r, s, t, u, v, w, x, y
+end
+
+
+function TestUndirectedGraph.testBFS()
+  local g, r, s, t, u, v, w, x, y = getUndirectedGraph()
+  g:breadthFirstSearch(s)
+
+  tester:asserteq(0, s.timestamp)
+  tester:asserteq(nil, s.parent)
+  tester:asserteq(Graph.state.FINISHED, s.state)
+
+  tester:asserteq(1, r.timestamp)
+  tester:asserteq(s, r.parent)
+  tester:asserteq(Graph.state.FINISHED, r.state)
+  
+  tester:asserteq(1, w.timestamp)
+  tester:asserteq(s, w.parent)
+  tester:asserteq(Graph.state.FINISHED, w.state)
+
+  tester:asserteq(2, v.timestamp)
+  tester:asserteq(r, v.parent)
+  tester:asserteq(Graph.state.FINISHED, v.state)
+
+  tester:asserteq(2, t.timestamp)
+  tester:asserteq(w, t.parent)
+  tester:asserteq(Graph.state.FINISHED, t.state)
+
+  tester:asserteq(2, x.timestamp)
+  tester:asserteq(w, x.parent)
+  tester:asserteq(Graph.state.FINISHED, x.state)
+
+  tester:asserteq(3, u.timestamp)
+  tester:assert(u.parent == t or u.parent == x)
+  tester:asserteq(Graph.state.FINISHED, u.state)
+
+  tester:asserteq(3, y.timestamp)
+  tester:asserteq(x, y.parent)
+  tester:asserteq(Graph.state.FINISHED, y.state)
+end
+
+function TestUndirectedGraph.testShortestPath()
+  local g, r, s, t, u, v, w, x, y = getUndirectedGraph()
+  local got = g:shortestPath(s, y)
+  tester:assertTableEq({s, w, x, y}, got)
+
+  got = g:shortestPath(s, t, true)
+  tester:assertTableEq({s, w, t}, got)
+
+  got = g:shortestPath(v, y)
+  tester:assertTableEq({v, r, s, w, x, y}, got)
+end
+
 
 tester = torch.Tester()
 tester:add(TestDirectedGraph)
