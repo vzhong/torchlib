6dd5fefb1330486e8c760f599a587da718c7a2c3
Victor Zhong <victor@victorzhong.com>
2016-05-08 00:22:25 -0700

    fix ordering issue

--- test/test_set.lua
+++ test/test_set.lua
@@ -145,8 +145,8 @@ function TestSet.testSubtract()
 end
 
 function TestSet.testToString()
-  local s = Set():addMany(5, 5)
-  tester:asserteq('tl.Set(5, 5)', tostring(s))
+  local s = Set():addMany(5)
+  tester:asserteq('tl.Set(5)', tostring(s))
 end
 
 function TestSet.testCopy()
